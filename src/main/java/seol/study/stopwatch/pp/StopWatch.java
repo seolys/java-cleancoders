5303a92cf0347e324bf83c8ccf7f86f9b47f8494
seol <seolnavy@naver.com>
2020-04-19 17:10:42 +0900

    컬렉션 상속 샘플추가

--- src/main/java/seol/study/stopwatch/pp/StopWatch.java
+++ src/main/java/seol/study/stopwatch/pp/StopWatch.java
@@ -7,6 +7,7 @@ public class StopWatch {
         return endTime - startTime;
     }
 
+    // 나노초 요건이 들어왔을때 변수 및 로직 추가
     public long startNanoTime;
     public long endNanoTime;
     public long getElapsedNanoTime() {
